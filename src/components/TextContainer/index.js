1948d6d85d66eaf9ee4566155cb0f50b1e23f22c
Ruiub3i <ruivaz9@hotmail.com>
2019-11-06 23:05:13 +0000

    add link

---
+++ src/components/TextContainer/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Typography } from "components";
+import "./index.css";
+
+class TextContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        let {  text, color, size, link } = this.props;
+
+        return (
+            <a href={link} styleName="text-container" target={'_blank'}>            
+                <Typography color={color} variant={size}>{text}</Typography>
+            </a>
+        );
+    }
+}
+
+
+
+export default TextContainer;
