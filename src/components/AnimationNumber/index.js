5a1d6c352c1129ff944a6bfae92bc49f9c76687b
Ruiub3i <ruivaz9@hotmail.com>
2019-11-19 14:49:13 +0000

    update

--- src/components/AnimationNumber/index.js
+++ src/components/AnimationNumber/index.js
@@ -5,25 +5,69 @@ import { Typography } from 'components';
 
 import "./index.css";
 
-const AnimationNumber = ({number, variant='h4', color='white', span=null}) => {
-    return (
-        <Typography variant={variant} color={color}>
-            <AnimatedNumber 
-                style={{
-                    transition: '0.4s ease-out',
-                    transitionProperty:
-                        'background-color, color, opacity'
-                }}
-                frameStyle={perc => (
-                    perc === 100 ? {} : {opacity: 1}
-                )}
-                duration={300}
-                value={number}
-            formatValue={n => Numbers.toFloat(n)}/>
-            <span style={{marginLeft : 4, fontSize : 18, opacity : 0.4}}>{span}</span>
-        </Typography>
+const defaultProps = {
+    number : 0,
+    variant : 'h4',
+    color :'white',
+    span : null 
+}
 
-)};
+class AnimationNumber extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = defaultProps;
+    }
+
+    componentDidMount(){
+        this.projectData(this.props)
+    }
+
+    componentWillReceiveProps(props){
+        this.projectData(props);
+    }
+    
+    projectData = async (props) => {
+        const { number, color, variant, span } = props;
+        if(this.state.number == props.number){return null};
+        this.setState({
+           number : number ? number : defaultProps.number,
+           color : color ? color : defaultProps.color,
+           variant : variant ? variant : defaultProps.variant,
+           span : span ? span : defaultProps.span
+       })
+        
+    }
+
+    shouldComponentUpdate(nextProps){
+        return this.state.number != nextProps.number;
+    }
+
+    render = () => {
+        const { number, variant='h4', color='white', span=null } = this.state;
+        console.log(number)
+        return (
+            <Typography variant={variant} color={color}>
+                <AnimatedNumber 
+                    style={{
+                        transition: '0.4s ease-out',
+                        transitionProperty:
+                            'background-color, color, opacity'
+                    }}
+                    frameStyle={perc => (
+                        perc === 100 ? {} : {opacity: 1}
+                    )}
+                    duration={500}
+                    value={number}
+                formatValue={n => Numbers.toFloat(n)}/>
+                <span style={{marginLeft : 4, fontSize : 18, opacity : 0.4}}>{span}</span>
+            </Typography>
+        );
+    }
+}
+
+
+
 
 
 
