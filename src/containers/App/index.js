1a67e1f74dfc65baf38ceb8f4a77c9d8fa88573e
Guilherme Santana <guilherme@betprotocol.com>
2020-03-12 17:03:52 -0300

    change route to /account/affiliate

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -519,7 +519,7 @@ class App extends Component {
                                                         path={`${url}/withdraws`} 
                                                         render={props => <AccountPage {...props} />} />
                                                     <Route 
-                                                        path={`${url}/referral`} 
+                                                        path={`${url}/affiliate`} 
                                                         render={props => <AccountPage {...props} />} />
                                                 </>
                                             )}
