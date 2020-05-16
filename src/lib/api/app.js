c2cfb4b4b281e8fe5cd0a66fe7adda07aefbd5df
Guilherme Santana <guilherme@betprotocol.com>
2020-05-16 00:52:44 -0300

    adjusts

--- src/lib/api/app.js
+++ src/lib/api/app.js
@@ -102,7 +102,7 @@ async function getPopularNumbers({size}) {
 
 async function ping() {
     try {
-        const response = await axios.post(`${apiUrl}/api/ping/post`, {
+        const response = await axios.post(`${apiUrl}/api/status/post`, {
             app: appId,
             type: "user"
         });
