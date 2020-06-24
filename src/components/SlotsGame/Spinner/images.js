c210cf6d26875537ae72a7ac36638b9d4f1e4594
alissonblaas <alissoblaas@gmail.com>
2020-06-24 04:35:06 -0300

    make some changes for number and add icons

---
+++ src/components/SlotsGame/Spinner/images.js
@@ -0,0 +1,21 @@
+import Cherry from "../../../assets/icons/SlotsIcons/cherry.png";
+import Apple from "../../../assets/icons/SlotsIcons/apple.png";
+import Bell from "../../../assets/icons/SlotsIcons/bell.png";
+import Cent from "../../../assets/icons/SlotsIcons/cent.png";
+import Fruit from "../../../assets/icons/SlotsIcons/fruit.png";
+import Heart from "../../../assets/icons/SlotsIcons/heart.png";
+import Leaf from "../../../assets/icons/SlotsIcons/leaf.png";
+import Seven from "../../../assets/icons/SlotsIcons/seven.png";
+
+const images = {
+  cherry: { Cherry },
+  apple: { Apple },
+  bell: { Bell },
+  cent: { Cent },
+  fruit: { Fruit },
+  heart: { Heart },
+  leaf: { Leaf },
+  seven: { Seven }
+};
+
+export default images;
