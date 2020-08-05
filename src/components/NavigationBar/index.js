e2f5131df4a80fe61dc80b2ea0ee65baa084ba86
Guilherme Santana <guilherme@betprotocol.com>
2020-08-05 12:33:56 -0300

    dynamic tabs

--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Typography } from 'components';
+import { getAppCustomization } from "../../lib/helpers";
+import { Link } from "react-router-dom";
+import classNames from 'classnames';
 import _ from 'lodash';
 import "./index.css";
-import { Typography, CasinoIcon } from 'components';
-import { Link } from "react-router-dom";
-import { CopyText } from '../../copy';
 
 class NavigationBar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           
+           tabs: [],
+           open: false
         };
     }
 
@@ -22,22 +24,82 @@ class NavigationBar extends Component {
         this.projectData(props);
     }
     
-    projectData = async (props) => {}
+    projectData = async (props) => {
+        const { topTab } = getAppCustomization();
+
+        this.setState({ tabs: topTab.ids });
+    }
+
+    renderMenuItem = ({link_url, icon, name}, isMainMenu=false) => {
+        return (
+            <Link to={link_url} styleName={isMainMenu == true ? 'navigation-step navigation-margin' : 'navigation-step'}>
+                <div styleName='img'>
+                    <img src={icon} width="22"/>
+                </div>
+                <div styleName='text'>
+                    <Typography variant={'small-body'} color={'white'}>
+                        {name}
+                    </Typography>
+                </div>
+            </Link>
+        )
+    }
+
+    onOpenMenu() {
+        const { open } = this.state;
+
+        this.setState({ open: !open })
+    }
 
     render() {
-        const {ln} = this.props;
-        const copy = CopyText.navigationBarIndex[ln];
+        const { tabs, open } = this.state;
+        const styles = classNames("dropdown-content", {
+            "dropdown-content-open": open == true
+        });
+
+
         return (
-                <Link to='/' styleName='navigation-step'>
-                    <div styleName='img'>
-                        <CasinoIcon/>
-                    </div>
-                    <div styleName='text'>
-                        <Typography variant={'small-body'} color={'white'}>
-                            {copy.INDEX.TYPOGRAPHY.TEXT[0]}
-                        </Typography>
-                    </div>
-                </Link>
+            <div styleName="tabs">
+                {
+                    tabs.slice(0, 2).map(t => {
+                        return (
+                            this.renderMenuItem({
+                                link_url: t.link_url,
+                                icon: t.icon,
+                                name: t.name
+                            }, true)
+                        )
+                    })
+                }
+                {
+                    tabs.length > 2
+                    ?
+                        <div styleName="dropdown">
+                            <a onClick={() => this.onOpenMenu()}>
+                                <div styleName="dropdown-dots"/>
+                            </a>
+                            <div styleName={styles}>
+                                <div styleName="dropdown-nav">
+                                    <div styleName="dropdown-column">
+                                        {
+                                            tabs.slice(2, tabs.length).map(t => {
+                                                return (
+                                                    this.renderMenuItem({
+                                                        link_url: t.link_url,
+                                                        icon: t.icon,
+                                                        name: t.name
+                                                    })
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    :
+                        null
+                }
+            </div>
         );
     }
 }
