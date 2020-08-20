0cdc9e2ad7fc8276a5b2cf15a31930035cb5c291
Guilherme Santana <guilherme@betprotocol.com>
2020-08-20 19:52:59 -0300

    Subsctions presentation

---
+++ src/components/SubSections/Positions/index.js
@@ -0,0 +1,9 @@
+import LayoutLeft from './LayoutLeft';
+import LayoutRight from './LayoutRight';
+import LayoutTop from './LayoutTop';
+import LayoutBottom from './LayoutBottom';
+
+export { LayoutLeft };
+export { LayoutRight };
+export { LayoutTop };
+export { LayoutBottom };
