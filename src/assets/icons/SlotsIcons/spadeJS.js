7879f8d776cf8e5f57cea2724782dbfe0b37b346
AlissonBlaas <alissoblaas@gmail.com>
2020-09-04 18:04:50 -0300

    fixed slots

---
+++ src/assets/icons/SlotsIcons/spadeJS.js
@@ -0,0 +1,48 @@
+import React from "react";
+// import propTypes from "prop-types";
+
+// import { Container } from './styles';
+
+const SpadeJS = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 133 128">
+      <defs>
+        <style>
+          .cls-1
+          {"fill:#8cb1f1;"}
+          .cls-2
+          {"fill:#b7ccf1;"}
+          .cls-3
+          {"fill:#367bf1;"}
+          .cls-4
+          {"fill:#c9b7f4;"}
+        </style>
+      </defs>
+      <title>queen</title>
+      <g id="Layer_2" data-name="Layer 2">
+        <path
+          className="cls-1"
+          d="M97.48,63.9,70,37.5,42.52,63.9a18.24,18.24,0,0,0,0,26.28,19.11,19.11,0,0,0,14.33,5.33l-4,6.92,34.26.07-4-7a19.07,19.07,0,0,0,14.31-5.33,18.24,18.24,0,0,0,0-26.28Z"
+        />
+        <path
+          className="cls-2"
+          d="M92.43,59c5,7.29,8.64,16.74,3.79,26a25.83,25.83,0,0,1-9.66,10.37,18.87,18.87,0,0,0,10.92-5.19,18.24,18.24,0,0,0,0-26.28Z"
+        />
+        <path
+          className="cls-3"
+          d="M93.83,67.66,70,44.78,46.18,67.66a13,13,0,0,0,0,18.76,13.91,13.91,0,0,0,19.29,0L69,83c-1.41,6.61-2,14.27-9.51,14.2l21,.06C73,97.33,72.39,89.6,71,83l3.55,3.44a13.91,13.91,0,0,0,19.29,0h0A13,13,0,0,0,93.83,67.66Z"
+        />
+        <path
+          className="cls-4"
+          d="M64.73,99.13,64,99S64.27,99.06,64.73,99.13Z"
+        />
+      </g>
+    </svg>
+  );
+};
+
+// Dog.propTypes = {
+//   pathSound: propTypes.string.isRequired
+// };
+
+export default SpadeJS;
