133eb5e3f6341c36a7f871369bcefadb94dc542a
AlissonBlaas <alissoblaas@gmail.com>
2020-09-21 03:37:23 -0300

    fix moonpay integration

--- src/components/WalletTab/index.js
+++ src/components/WalletTab/index.js
@@ -1,234 +1,340 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Tabs, DepositForm, WithdrawForm, Typography, Button, EmailIcon } from "components";
+import {
+  Tabs,
+  DepositForm,
+  WithdrawForm,
+  Typography,
+  Button,
+  EmailIcon
+} from "components";
+import { Col, Row } from "reactstrap";
+import _ from "lodash";
 import PaymentBox from "../PaymentBox";
 import DepositList from "./DepositList";
 import WithdrawList from "./WithdrawList";
-import { CopyText } from '../../copy';
+import { CopyText } from "../../copy";
 import { getApp, getAppCustomization } from "../../lib/helpers";
-import { Col, Row } from 'reactstrap';
 import { setMessageNotification } from "../../redux/actions/message";
 import store from "../../containers/App/store";
-import _ from 'lodash';
-import './index.css';
+import "./index.css";
 
 const defaultState = {
-    tab: "deposit",
-    wallets : [],
-    wallet: null,
-    isEmailConfirmed: false,
-    isConfirmationSent: false
-}
+  tab: "deposit",
+  wallets: [],
+  wallet: null,
+  isEmailConfirmed: false,
+  isConfirmationSent: false,
+  onOpenMoonpay: false
+};
 
-class WalletTab extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = defaultState;
+class WalletTab extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = defaultState;
+  }
+
+  componentDidMount() {
+    const { isCurrentPath } = this.props;
+
+    if (isCurrentPath) {
+      this.projectData(this.props);
     }
 
-    componentDidMount(){
-        const { isCurrentPath } = this.props;
+    console.log(getApp());
+  }
 
-        if (isCurrentPath) {
-            this.projectData(this.props);
-        }
-        console.log(getApp());
+  componentWillReceiveProps(props) {
+    const { isCurrentPath } = props;
+
+    if (props !== this.props && isCurrentPath) {
+      this.projectData(props);
+    }
+  }
+
+  projectData = async props => {
+    const { profile } = this.props;
+    let { wallet } = this.state;
+    const user = !_.isEmpty(props.profile) ? props.profile : null;
+    const virtual = getApp().virtual;
+    const wallets =
+      virtual === true
+        ? profile
+            .getWallets()
+            .filter(
+              w =>
+                new String(w.currency.ticker).toString().toLowerCase() !== "eth"
+            )
+        : profile.getWallets();
+
+    if (wallets && !wallet) {
+      wallet = wallets.find(w => w.currency.virtual === false);
     }
 
-    componentWillReceiveProps(props){
-        const { isCurrentPath } = props;
-        if(props !== this.props && isCurrentPath) {
-            this.projectData(props);
-        }
+    this.setState({
+      ...this.state,
+      wallets,
+      wallet,
+      virtual: getApp().virtual,
+      isEmailConfirmed: await user.isEmailConfirmed()
+    });
+  };
+
+  handleTabChange = name => {
+    this.setState({ tab: name });
+  };
+
+  handleOpenMoonpay = () => {
+    this.setState({ onOpenMoonpay: true });
+  };
+
+  handleOpenMoonpayFalse = () => {
+    this.setState({ onOpenMoonpay: false });
+  };
+
+  handleMoonpay = () => {
+    const { profile } = this.props;
+    const userEmail = profile.user.email;
+    const userId = profile.user.id;
+    const skin = getAppCustomization().skin.skin_type;
+
+    const resultMoonpay = getApp().integrations.moonpay;
+
+    console.log(getApp());
+
+    return (
+      <div>
+        <div styleName="bg-cnt">
+          <div styleName="bg-cnt-2">
+            <button onClick={() => this.handleOpenMoonpayFalse()} type="button" styleName="button-x">
+              <Typography color="secondary" variant="h2">
+                X
+              </Typography>
+            </button>
+            <iframe
+              styleName="bg-moonpay-box"
+              allow="accelerometer; autoplay; camera; gyroscope; payment"
+              frameBorder="0"
+              src={`https://buy-staging.moonpay.io?apiKey=${resultMoonpay.key}&currencyCode=eth&walletAddress=&colorCode=%239898F0&email=${userEmail}&externalCustomerId=${userId}`}
+            >
+              <p>Your browser does not support iframes.</p>
+            </iframe>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  changeWallet = async wallet => {
+    this.setState({ wallet });
+  };
+
+  handleAddress = address => {
+    const { wallet } = this.state;
+
+    if (wallet) {
+      wallet.address = address;
+      this.setState({ wallet });
     }
+  };
 
-    projectData = async (props) => {
-        const { profile } = this.props;
-        let { wallet } = this.state;
-        var user = !_.isEmpty(props.profile) ? props.profile : null;
-        const virtual = getApp().virtual;
-        const wallets = virtual === true ? profile.getWallets().filter(w => new String(w.currency.ticker).toString().toLowerCase() !== 'eth') : profile.getWallets();
+  handleResendConfirmEmail = async () => {
+    const { profile, ln } = this.props;
+    const copy = CopyText.homepage[ln];
 
-        if(wallets && !wallet) {
-            wallet = wallets.find(w => w.currency.virtual === false);
-        }
+    try {
+      this.setState({ isConfirmationSent: true });
+      const res = await profile.resendConfirmEmail();
+      const { message, status } = res.data;
 
-        this.setState({...this.state,
-            wallets,
-            wallet,
-            virtual : getApp().virtual,
-            isEmailConfirmed: await user.isEmailConfirmed()
-        });
+      if (status != 200) {
+        store.dispatch(setMessageNotification(message));
+        throw message;
+      }
+
+      store.dispatch(
+        setMessageNotification(copy.CONTAINERS.APP.NOTIFICATION[2])
+      );
+      this.setState({ isConfirmationSent: false });
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    handleTabChange = name => {
-        this.setState({ tab: name });
-    };
+  renderPopSendEmailAlert = () => {
+    const { ln } = this.props;
+    const { isEmailConfirmed, isConfirmationSent } = this.state;
+    const copyConfirmEmail = CopyText.homepage[ln];
+    const skin = getAppCustomization().skin.skin_type;
+    const resultMoonpay = getApp().integrations.moonpay;
 
-    handleMoonpay = () => {
-        const resultMoonpay = getApp().integrations.moonpay;
+    return isEmailConfirmed === false ? (
+      <div styleName="email-confirmation">
+        <div styleName="email-title">
+          <span styleName="icon">
+            <EmailIcon />
+          </span>
+          <Typography variant="small-body" color="grey" weight="bold">
+            {copyConfirmEmail.CONTAINERS.APP.MODAL[2]}
+          </Typography>
+        </div>
+        <div styleName="email-content">
+          <div styleName="email-text">
+            <Typography variant="x-small-body" color="white">
+              Your e-mail is not confirmed.
+            </Typography>
+            <Typography variant="x-small-body" color="white">
+              Please click the button to send another e-mail confirmation.
+            </Typography>
+          </div>
+          <div styleName="email-buttons">
+            <div styleName="button">
+              <Button
+                size="x-small"
+                theme="action"
+                disabled={isConfirmationSent}
+                onClick={this.handleResendConfirmEmail}
+              >
+                <Typography
+                  color={skin == "digital" ? "secondary" : "fixedwhite"}
+                  variant="small-body"
+                >
+                  {copyConfirmEmail.CONTAINERS.APP.MODAL[2]}
+                </Typography>
+              </Button>
 
-        window.moonpayWidget.initialize({
-            apiKey: resultMoonpay.key,
-            colorCode: '#f36a3d',
-            cryptoCurrencies: ['eth'],
-            fiatCurrencies: ['usd'],
-            selector: '#moonpay_widget',
-          });
-    }
-
-    changeWallet = async (wallet) => {
-        this.setState({ wallet });
-    }
-
-    handleAddress = address => {
-        let { wallet } = this.state;
-
-        if(wallet) {
-            wallet.address = address;
-            this.setState({ wallet });
-        }
-    };
-
-    handleResendConfirmEmail = async () => {
-        const { profile, ln } = this.props;
-        const copy = CopyText.homepage[ln];
-
-        try{
-            this.setState({ isConfirmationSent : true });
-            let res = await profile.resendConfirmEmail();
-            let { message, status } = res.data;        
-
-            if(status != 200){
-                store.dispatch(setMessageNotification(message));
-                throw message
-            };
-
-            store.dispatch(setMessageNotification(copy.CONTAINERS.APP.NOTIFICATION[2]));
-            this.setState({ isConfirmationSent : false });
-
-        } catch(err){
-            console.log(err);
-        }
-
-    };
-
-    renderPopSendEmailAlert = () => {
-        const { ln } = this.props;
-        const { isEmailConfirmed, isConfirmationSent } = this.state;
-        const copyConfirmEmail = CopyText.homepage[ln];
-        const skin = getAppCustomization().skin.skin_type;
-
-        return(
-            isEmailConfirmed === false
-            ?
-                <div styleName="email-confirmation">
-                    <div styleName="email-title">
-                        <span styleName="icon">
-                            <EmailIcon/>
-                        </span>
-                        <Typography variant={'small-body'} color={'grey'} weight={"bold"}>
-                            {copyConfirmEmail.CONTAINERS.APP.MODAL[2]}
-                        </Typography>
-                    </div>
-                    <div styleName="email-content">
-                        <div styleName="email-text">
-                            <Typography variant={'x-small-body'} color={'white'}>
-                                Your e-mail is not confirmed.
-                            </Typography>
-                            <Typography variant={'x-small-body'} color={'white'}>
-                                Please click the button to send another e-mail confirmation.
-                            </Typography>
-                        </div>
-                        <div styleName="email-buttons">
-                            <div styleName="button">
-                                <Button size={'x-small'} theme={'action'} disabled={isConfirmationSent} onClick={this.handleResendConfirmEmail}>
-                                    <Typography color={skin == "digital" ? 'secondary' : 'fixedwhite'} variant={'small-body'}>{copyConfirmEmail.CONTAINERS.APP.MODAL[2]}</Typography>
-                                </Button>
-                            </div>
-                        </div>
-                    </div>
+              {resultMoonpay.isValid ? (
+                <div styleName="button">
+                  <Button
+                    size="x-small"
+                    theme="action"
+                    onClick={this.handleMoonpay}
+                  >
+                    <Typography
+                      color={skin == "digital" ? "secondary" : "fixedwhite"}
+                      variant="small-body"
+                    >
+                      {"Buy with creditcard"}
+                    </Typography>
+                  </Button>
                 </div>
-            :
-                null  
-        )
-    }
-
-    render(){
-        const { ln, isCurrentPath } = this.props;
-        const { tab, wallets, wallet, virtual, isEmailConfirmed } = this.state;
-        const copy = CopyText.cashierFormIndex[ln];
-        const skin = getAppCustomization().skin.skin_type;
-        if(!wallet) { return null };
-
-        return (
-            <div>
-                <Row styleName={isEmailConfirmed === false ? "blur" : null}>
-                    <Col md={12} lg={12} xl={4}>
-                        <div>
-                            {wallets.map( w => {
-                                return (
-                                    <PaymentBox 
-                                        onClick={() => this.changeWallet(w)}
-                                        isPicked={new String(wallet.currency._id).toString() == new String(w.currency._id).toString()}
-                                        wallet={w}
-                                    />
-                                )
-                            })}
-                        </div>
-                    </Col>
-                    <Col md={12} lg={12} xl={8}>
-                        <div>
-                            <Tabs
-                                selected={tab}
-                                options={[
-                                {
-                                    value: "deposit",
-                                    label: virtual === true ? copy.INDEX.TABS.LABEL[2] : copy.INDEX.TABS.LABEL[0]
-                                },
-                                {   
-                                    value: "withdraw", 
-                                    label: copy.INDEX.TABS.LABEL[1],
-                                    disabled: virtual  === true
-                                }
-                                ]}
-                                onSelect={this.handleTabChange}
-                                style="full-background"
-                            />
-                        </div>
-                        {
-                        isEmailConfirmed === true
-                        ?
-                            tab === "deposit" ? 
-                            <>
-                                <div>
-                                    <DepositForm  wallet={wallet} onAddress={this.handleAddress}/>
-                                    <DepositList isCurrentPath={isCurrentPath} />
-                                </div> 
-                                <div styleName="button">
-                                    <Button size={'x-small'} theme={'action'} onClick={this.handleMoonpay}>
-                                        <Typography color={skin == "digital" ? 'secondary' : 'fixedwhite'} variant={'small-body'}>{"Buy with creditcard"}</Typography>
-                                    </Button>
-                                </div>
-                            </>
-                            : 
-                                <div><WithdrawForm wallet={wallet} onAddress={this.handleAddress}/> <WithdrawList isCurrentPath={isCurrentPath} /></div>
-                        :
-                            null
-                        }
-                    </Col>
-                </Row>
-                {this.renderPopSendEmailAlert()}
+              ) : null}
             </div>
-        )
+          </div>
+        </div>
+      </div>
+    ) : null;
+  };
+
+  render() {
+    const { ln, isCurrentPath } = this.props;
+    const {
+      tab,
+      wallets,
+      wallet,
+      virtual,
+      isEmailConfirmed,
+      onOpenMoonpay
+    } = this.state;
+    const copy = CopyText.cashierFormIndex[ln];
+    const skin = getAppCustomization().skin.skin_type;
+
+    if (!wallet) {
+      return null;
     }
+
+    return (
+      <>
+        <div>
+          <Row styleName={isEmailConfirmed === false ? "blur" : null}>
+            <Col md={12} lg={12} xl={4}>
+              <div>
+                {wallets.map(w => {
+                  return (
+                    <PaymentBox
+                      onClick={() => this.changeWallet(w)}
+                      isPicked={
+                        new String(wallet.currency._id).toString() ==
+                        new String(w.currency._id).toString()
+                      }
+                      wallet={w}
+                    />
+                  );
+                })}
+              </div>
+            </Col>
+            <Col md={12} lg={12} xl={8}>
+              <div>
+                <Tabs
+                  selected={tab}
+                  options={[
+                    {
+                      value: "deposit",
+                      label:
+                        virtual === true
+                          ? copy.INDEX.TABS.LABEL[2]
+                          : copy.INDEX.TABS.LABEL[0]
+                    },
+                    {
+                      value: "withdraw",
+                      label: copy.INDEX.TABS.LABEL[1],
+                      disabled: virtual === true
+                    }
+                  ]}
+                  onSelect={this.handleTabChange}
+                  style="full-background"
+                />
+              </div>
+              {isEmailConfirmed === true ? (
+                tab === "deposit" ? (
+                  <>
+                    <div>
+                      <DepositForm
+                        wallet={wallet}
+                        onAddress={this.handleAddress}
+                      />
+                      <DepositList isCurrentPath={isCurrentPath} />
+                    </div>
+                    <div styleName="button">
+                      <Button
+                        size="x-small"
+                        theme="action"
+                        onClick={() => this.handleOpenMoonpay()}
+                      >
+                        <Typography
+                          color={skin == "digital" ? "secondary" : "fixedwhite"}
+                          variant="small-body"
+                        >
+                          {"Buy with creditcard"}
+                        </Typography>
+                      </Button>
+                    </div>
+                  </>
+                ) : (
+                  <div>
+                    <WithdrawForm
+                      wallet={wallet}
+                      onAddress={this.handleAddress}
+                    />{" "}
+                    <WithdrawList isCurrentPath={isCurrentPath} />
+                  </div>
+                )
+              ) : null}
+            </Col>
+          </Row>
+          {this.renderPopSendEmailAlert()}
+        </div>
+        {onOpenMoonpay === true ? this.handleMoonpay() : null}
+      </>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    return {
-        profile : state.profile,
-        ln: state.language
-    };
+function mapStateToProps(state) {
+  return {
+    profile: state.profile,
+    ln: state.language
+  };
 }
 
 export default connect(mapStateToProps)(WalletTab);
