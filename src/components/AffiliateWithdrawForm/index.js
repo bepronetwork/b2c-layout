cf086ab6cef4aa268d1013d55952ade2e2926982
João Victor <joao.victor@betprotocol.com>
2020-11-03 04:46:06 -0300

    Issue #518: centering the banner button when it has no title or subtitle

--- src/components/AffiliateWithdrawForm/index.js
+++ src/components/AffiliateWithdrawForm/index.js
@@ -2,62 +2,65 @@ import React, { Component } from "react";
 import { Tabs, Modal } from "components";
 import Withdraw from "./Withdraw";
 import { connect } from "react-redux";
-
 import "./index.css";
 import store from "../../containers/App/store";
 import { setModal } from "../../redux/actions/modal";
-import { CopyText } from '../../copy';
+import { CopyText } from "../../copy";
 
 class AffiliateWithdrawForm extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            tab: "withdraw"
-        };
-    }
-
-    handleTabChange = name => {
-        this.setState({ tab: name });
+    this.state = {
+      tab: "withdraw"
     };
+  }
 
-    onClose = async () => {
-        await store.dispatch(setModal({key : 'AffiliateWithdrawForm', value : false}));
+  handleTabChange = name => {
+    this.setState({ tab: name });
+  };
+
+  onClose = async () => {
+    await store.dispatch(
+      setModal({ key: "AffiliateWithdrawForm", value: false })
+    );
+  };
+
+  render() {
+    const { modal } = this.props;
+    const { tab } = this.state;
+    const { ln } = this.props;
+    const copy = CopyText.affiliateWithdrawFormIndex[ln];
+    if (!modal.AffiliateWithdrawForm) {
+      return null;
     }
 
-    render() {
-        const { modal } = this.props;
-        const { tab } = this.state;
-        const {ln} = this.props;
-        const copy = CopyText.affiliateWithdrawFormIndex[ln];
-        if(!modal.AffiliateWithdrawForm){ return null };
-
-        return (
-            <Modal onClose={this.onClose}>
-                <div styleName="root" style={{ overflowY: 'auto', overflowX : 'hidden'}}>
-                    <div styleName="tabs">
-                        <Tabs
-                            selected={tab}
-                            options={[
-                            { value: "withdraw", label: copy.INDEX.TABS.LABEL[0] }
-                            ]}
-                            onSelect={this.handleTabChange}
-                        />
-                    </div>
-                    <Withdraw onClose={this.onClose}/>
-                </div>
-            </Modal>
-        );
-    }
+    return (
+      <Modal onClose={this.onClose}>
+        <div
+          styleName="root"
+          style={{ overflowY: "auto", overflowX: "hidden" }}
+        >
+          <div styleName="tabs">
+            <Tabs
+              selected={tab}
+              options={[{ value: "withdraw", label: copy.INDEX.TABS.LABEL[0] }]}
+              onSelect={this.handleTabChange}
+            />
+          </div>
+          <Withdraw onClose={this.onClose} />
+        </div>
+      </Modal>
+    );
+  }
 }
 
-
-function mapStateToProps(state){
-    return {
-        profile : state.profile,
-        modal : state.modal,
-        ln: state.language
-    };
+function mapStateToProps(state) {
+  return {
+    profile: state.profile,
+    modal: state.modal,
+    ln: state.language
+  };
 }
 
 export default connect(mapStateToProps)(AffiliateWithdrawForm);
--- src/components/Banners/index.js
+++ src/components/Banners/index.js
@@ -1,121 +1,170 @@
 import React, { Component } from "react";
-import Carousel from 'react-bootstrap/Carousel'
+import Carousel from "react-bootstrap/Carousel";
 import { Typography, Button } from "components";
 import { connect } from "react-redux";
+import classNames from "classnames";
+import _ from "lodash";
 import { getAppCustomization } from "../../lib/helpers";
-import classNames from 'classnames';
-import _ from 'lodash';
 import "./index.css";
 
 class Banners extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      banners: [],
+      index: 0,
+      isFullWidth: false
+    };
+    this.handleSelect = this.handleSelect.bind(this);
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            banners : [],
-            index : 0,
-            isFullWidth : false
-        }
+  componentDidMount() {
+    this.projectData(this.props);
+  }
+
+  componentWillReceiveProps(props) {
+    this.projectData(props);
+  }
+
+  projectData = async props => {
+    const { ln } = props;
+    let { banners } = getAppCustomization();
+
+    banners = banners.languages.find(
+      ({ language }) =>
+        language.isActivated === true && language.prefix === ln.toUpperCase()
+    );
+
+    this.setState({
+      banners: !_.isEmpty(banners) ? banners.ids : null,
+      isFullWidth: !_.isEmpty(banners) ? banners.fullWidth : false
+    });
+  };
+
+  handleSelect(selectedIndex) {
+    this.setState({ index: selectedIndex });
+  }
+
+  handleClick(url) {
+    window.location.assign(url);
+  }
+
+  render() {
+    const { banners, index, isFullWidth } = this.state;
+
+    if (_.isEmpty(banners)) {
+      return null;
     }
 
-    componentDidMount(){
-        this.projectData(this.props);
-    }
+    const skin = getAppCustomization().skin.skin_type;
+    const bannersStyles = classNames("banners", {
+      "banners-full-width": isFullWidth
+    });
 
-    componentWillReceiveProps(props){
-        this.projectData(props);
-    }
+    return (
+      <div styleName={bannersStyles}>
+        <Carousel
+          activeIndex={index}
+          onSelect={this.handleSelect}
+          pause="hover"
+        >
+          {banners.map(
+            ({ title, subtitle, image_url, link_url, button_text }) => {
+              const styles = classNames("text-image", {
+                "text-image-show": !(title || subtitle)
+              });
+              const bannerStyles = classNames("banner", {
+                "banner-full": isFullWidth
+              });
+              const textStyles = classNames("text", {
+                "text-full": isFullWidth,
+                "no-text": isFullWidth && !title && !subtitle
+              });
 
-    projectData = async (props) => {
-        const { ln } = props;
-        let { banners } = getAppCustomization();
-
-        banners = banners.languages.find(b => b.language.isActivated === true && b.language.prefix === ln.toUpperCase());
-
-        this.setState({ 
-            banners : !_.isEmpty(banners) ? banners.ids : null,
-            isFullWidth : !_.isEmpty(banners) ? banners.fullWidth : false
-        })
-    }
-
-    handleSelect(selectedIndex, e) {
-        this.setState({index : selectedIndex});
-    }
-    
-    handleClick(url) {
-        window.location.assign(url);
-    }
-
-    render() {
-        const { banners, index, isFullWidth } = this.state;
-        
-        if(_.isEmpty(banners)) { return null; }
-
-        const skin = getAppCustomization().skin.skin_type;
-        const bannersStyles = classNames("banners", {
-            "banners-full-width": isFullWidth
-        });
-
-        return (
-            <div styleName={bannersStyles}>
-              <Carousel activeIndex={index} onSelect={this.handleSelect.bind(this)} pause="hover">
-                    {banners.map(banner => {
-                        const styles = classNames("text-image", {"text-image-show": !(banner.title || banner.subtitle)});
-                        const bannerStyles = classNames("banner", { "banner-full": isFullWidth });
-                        const textStyles = classNames("text", { "text-full": isFullWidth, "no-text": isFullWidth && !banner.title && !banner.subtitle });
-                        return (
-                            <Carousel.Item>
-                                <div styleName={!banner.title && !banner.subtitle ?"banner-without-text" : bannerStyles} style={{background: isFullWidth == true || (isFullWidth == false && !banner.title && !banner.subtitle) ? "url("+banner.image_url+") center center / cover no-repeat" : null}}>
-                                    <div styleName={textStyles}>
-                                        {
-                                            banner.title || banner.subtitle || banner.button_text ?
-                                                <div styleName="banner-content">
-                                                    <div styleName="fields">
-                                                        <Typography color={'white'} variant={'h3'} weight={'bold'}>{banner.title}</Typography>
-                                                    </div>
-
-                                                    <div styleName="fields fields-text">
-                                                        <Typography color={'white'} variant={'small-body'}>{banner.subtitle}</Typography>
-                                                    </div>
-                                                    
-                                                    {banner.button_text &&  banner.link_url ?
-                                                        <Button  onClick={() => this.handleClick(banner.link_url)} theme="action">
-                                                            <Typography color={skin == "digital" ? "secondary" : "fixedwhite"} variant={'small-body'}>{banner.button_text}</Typography>
-                                                        </Button>
-                                                    : 
-                                                        null
-                                                    }
-                                                </div>
-                                            :
-                                                <div/>
-                                        }
-                                    </div>
-                                    <div styleName="image" style={{background: (isFullWidth == false && (banner.title || banner.subtitle)) ? "url("+banner.image_url+") center center / cover no-repeat" : banner.title || banner.subtitle ? "linear-gradient(to right, rgba(0,0,0,0.9) 0%,rgba(0,0,0,0) 69%)" : null }}>
-                                        <div styleName={styles}>
-                                            {
-                                                banner.title || banner.subtitle || banner.button_text ?
-                                                    <div/>
-                                                :
-                                                    <div/>
-                                            }
-                                        </div>
-                                    </div>
-                                </div>
-                            </Carousel.Item>
-                        )
-                    })}
-                </Carousel>
-            </div>
-        );
-      }
-    
+              return (
+                <Carousel.Item>
+                  <div
+                    styleName={
+                      !title && !subtitle ? "banner-without-text" : bannerStyles
+                    }
+                    style={{
+                      background:
+                        isFullWidth || (!isFullWidth && !title && !subtitle)
+                          ? `url(${image_url}) center center / cover no-repeat`
+                          : null
+                    }}
+                  >
+                    <div styleName={textStyles}>
+                      {title || subtitle || button_text ? (
+                        <div>
+                          <div styleName="fields">
+                            <Typography
+                              color="white"
+                              variant="h3"
+                              weight="bold"
+                            >
+                              {title}
+                            </Typography>
+                          </div>
+                          <div styleName="fields fields-text">
+                            <Typography color="white" variant="small-body">
+                              {subtitle}
+                            </Typography>
+                          </div>
+                          {button_text && link_url ? (
+                            <Button
+                              onClick={() => this.handleClick(link_url)}
+                              theme="action"
+                            >
+                              <Typography
+                                color={
+                                  skin === "digital"
+                                    ? "secondary"
+                                    : "fixedwhite"
+                                }
+                                variant="small-body"
+                              >
+                                {button_text}
+                              </Typography>
+                            </Button>
+                          ) : null}
+                        </div>
+                      ) : (
+                        <div />
+                      )}
+                    </div>
+                    <div
+                      styleName="image"
+                      style={{
+                        background:
+                          // eslint-disable-next-line no-nested-ternary
+                          !isFullWidth && (title || subtitle)
+                            ? `url(${image_url}) center center / cover no-repeat`
+                            : title || subtitle
+                            ? "linear-gradient(to right, rgba(0,0,0,0.9) 0%,rgba(0,0,0,0) 69%)"
+                            : null
+                      }}
+                    >
+                      <div styleName={styles}>
+                        {title || subtitle || button_text ? <div /> : <div />}
+                      </div>
+                    </div>
+                  </div>
+                </Carousel.Item>
+              );
+            }
+          )}
+        </Carousel>
+      </div>
+    );
+  }
 }
 
-function mapStateToProps(state){
-    return {
-        profile: state.profile,
-        ln: state.language
-    };
+function mapStateToProps(state) {
+  return {
+    profile: state.profile,
+    ln: state.language
+  };
 }
 
 export default connect(mapStateToProps)(Banners);
