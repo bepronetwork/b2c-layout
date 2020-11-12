0a00fe13859abd2e346a61a0ac9486c9b98e3b54
João Victor <joao.victor@betprotocol.com>
2020-11-12 15:54:46 -0300

    feat: adding multi-language to [GameCounter]

--- src/components/GameCounter/index.js
+++ src/components/GameCounter/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import { LinearProgress } from "@material-ui/core";
 import { Typography, Button } from "components";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { CopyText } from "../../copy";
 import "./index.css";
 
 // GameCounter is just a dumb component to show how many
 // games are being shown on the page
-const GameCounter = ({ quantity, total, ...props }) =>
+const GameCounter = ({ quantity, total, ln, ...props }) =>
   quantity < total ? (
     <div styleName="progress">
       <div styleName="line">
@@ -17,12 +19,15 @@ const GameCounter = ({ quantity, total, ...props }) =>
       </div>
       <div styleName="text">
         <Typography variant="x-small-body" color="white">
-          {`Displaying ${quantity} of ${total} games`}
+          {CopyText.gameCounter[ln].DESCRIPTION.replace(
+            /quantity/gi,
+            quantity
+          ).replace(/total/gi, total)}
         </Typography>
       </div>
       <Button size="x-small" theme="action" {...props}>
         <Typography color="white" variant="small-body">
-          Load More
+          {CopyText.gameCounter[ln].BUTTON}
         </Typography>
       </Button>
     </div>
@@ -38,4 +43,10 @@ GameCounter.defaultProps = {
   total: 0
 };
 
-export default GameCounter;
+function mapStateToProps(state) {
+  return {
+    ln: state.language
+  };
+}
+
+export default connect(mapStateToProps)(GameCounter);
