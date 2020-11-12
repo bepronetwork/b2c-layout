03d6aef6a568ffaa90811fde9aaa97ed3e3ff347
Rui Pedro <ruivaz1997@gmail.com>
2020-11-12 12:29:45 +0000

    Update index.jsx

--- src/components/Esports/Matches/index.jsx
+++ src/components/Esports/Matches/index.jsx
@@ -51,8 +51,10 @@ class Matches extends Component {
 
     renderMatch(match, beginDate) {
         const { games } = this.props;
-        const gameImage = games.find(g => g.external_id === match.videogame.id).image;
-
+        const game = games.find(g => g.external_id === match.videogame.id);
+        if(!game){return null};
+        const gameImage = game.image;
+        
         return (
             <div>
                 {
