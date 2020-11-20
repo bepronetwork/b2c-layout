8a6853fb1f8d02bfbf44fb880162d44d41a98671
alissonblaas <alissoblaas@gmail.com>
2020-11-20 14:56:41 -0300

    make some change

--- src/components/WheelGameOptions/index.jsx
+++ src/components/WheelGameOptions/index.jsx
@@ -299,7 +299,7 @@ class WheelGameOptions extends Component {
         }
 
         if (value === "0.5") {
-            newAmount = newAmount <= 0.000001 ? 0 : newAmount * 0.5;
+            newAmount = newAmount <= 0.00001 ? 0 : newAmount * 0.5;
         }
 
         if (newAmount > balance) {
