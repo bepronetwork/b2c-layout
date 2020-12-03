e95cfda62d578ccc005997dd15885b464e4637f2
João Victor Nunes <soujvnunes@gmail.com>
2020-12-03 14:37:10 -0300

    Converting the multiplier to functional component.

--- src/components/MultiplyMaxButton/index.jsx
+++ src/components/MultiplyMaxButton/index.jsx
@@ -1,37 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { isEmpty } from "lodash";
 import Typography from "../Typography";
 import { CopyText } from "../../copy";
-import { connect } from "react-redux";
 import { getAppCustomization } from "../../lib/helpers";
 import "./index.css";
 import gameOperations from "../../utils/gameOperations";
-import { isEmpty } from "lodash";
 
-const propTypes = {
-  onResult: PropTypes.func.isRequired,
-  onBetAmount: PropTypes.func.isRequired,
-  amount: PropTypes.number.isRequired,
-  language: PropTypes.string.isRequired,
-  profile: PropTypes.objectOf([PropTypes.object]).isRequired
-};
-
-class MultiplyMaxButton extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    const { profile, onResult, amount, onBetAmount } = this.props;
-    const { name } = event.currentTarget;
-    let newAmount;
-
+export const MultiplyMaxButton = ({
+  profile,
+  onResult,
+  amount,
+  onBetAmount,
+  language
+}) => {
+  const copy = CopyText.multiplyMaxButtonIndex[language];
+  const skin = getAppCustomization().skin.skin_type;
+  const handleClick = event => {
     if (isEmpty(profile)) {
       return null;
     }
 
+    const { name } = event.currentTarget;
     const balance = parseFloat(profile.getBalanceWithBonus());
+    let newAmount;
 
     newAmount = gameOperations(name, amount, balance);
 
@@ -44,72 +37,67 @@ class MultiplyMaxButton extends Component {
     }
 
     return onResult(newAmount);
-  }
+  };
 
-  render() {
-    const { language } = this.props;
-    const copy = CopyText.multiplyMaxButtonIndex[language];
-    const skin = getAppCustomization().skin.skin_type;
-
-    return (
-      <div styleName="root">
-        <div styleName="container">
-          <button
-            name={0.5}
-            onClick={this.handleClick}
-            styleName="button"
-            type="button"
-          >
-            <div styleName="button-container">
-              <Typography
-                weight="semi-bold"
-                variant="small-body"
-                color={skin == "digital" ? "secondary" : "casper"}
-              >
-                ½
-              </Typography>
-            </div>
-          </button>
-          <button
-            name={2}
-            onClick={this.handleClick}
-            styleName="button"
-            type="button"
-          >
-            <div styleName="button-container">
-              <Typography
-                weight="semi-bold"
-                variant="small-body"
-                color={skin == "digital" ? "secondary" : "casper"}
-              >
-                2×
-              </Typography>
-            </div>
-          </button>
-          <button
-            name="max"
-            onClick={this.handleClick}
-            styleName="button"
-            type="button"
-          >
-            <div styleName="button-container">
-              <Typography
-                weight="semi-bold"
-                variant="x-small-body"
-                color={skin == "digital" ? "secondary" : "casper"}
-              >
-                {copy.INDEX.TYPOGRAPHY.TEXT[0]}
-              </Typography>
-            </div>
-          </button>
-        </div>
+  return (
+    <div styleName="root">
+      <div styleName="container">
+        <button
+          name={0.5}
+          onClick={handleClick}
+          styleName="button"
+          type="button"
+        >
+          <div styleName="button-container">
+            <Typography
+              weight="semi-bold"
+              variant="small-body"
+              color={skin === "digital" ? "secondary" : "casper"}
+            >
+              ½
+            </Typography>
+          </div>
+        </button>
+        <button name={2} onClick={handleClick} styleName="button" type="button">
+          <div styleName="button-container">
+            <Typography
+              weight="semi-bold"
+              variant="small-body"
+              color={skin === "digital" ? "secondary" : "casper"}
+            >
+              2×
+            </Typography>
+          </div>
+        </button>
+        <button
+          name="max"
+          onClick={handleClick}
+          styleName="button"
+          type="button"
+        >
+          <div styleName="button-container">
+            <Typography
+              weight="semi-bold"
+              variant="x-small-body"
+              color={skin === "digital" ? "secondary" : "casper"}
+            >
+              {copy.INDEX.TYPOGRAPHY.TEXT[0]}
+            </Typography>
+          </div>
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+MultiplyMaxButton.propTypes = {
+  onResult: PropTypes.func.isRequired,
+  onBetAmount: PropTypes.func.isRequired,
+  amount: PropTypes.number.isRequired,
+  language: PropTypes.string.isRequired,
+  profile: PropTypes.objectOf([PropTypes.object]).isRequired
+};
 
 const mapStateToProps = ({ profile, language }) => ({ profile, language });
-
-MultiplyMaxButton.propTypes = propTypes;
 
 export default connect(mapStateToProps)(MultiplyMaxButton);
